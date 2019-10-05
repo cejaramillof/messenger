71db37737fa5bb20026b6c54a97aaefaacc5027f
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-05 15:01:10 -0500

    create styles for login home and conversation views

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,6 +1,7 @@
 export interface User {
   nick: string;
   subnick?: string; // ? make optional
+  status: string;
   age: number;
   email: string;
   friend: boolean;
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,7 +14,8 @@ export class UserService {
       age: 23,
       email: 'cejaramillof@gmail.com',
       friend: true,
-      uid: 1
+      uid: 1,
+      status: 'offline'
     };
 
     let user1: User = {
@@ -22,35 +23,40 @@ export class UserService {
       age: 24,
       email: 'ed@aoe.aoe',
       friend: true,
-      uid: 1
+      uid: 1,
+      status: 'online'
     };
     let user2: User = {
       nick: 'Freddy',
       age: 28,
       email: 'fred@aoe.aoe',
       friend: true,
-      uid: 2
+      uid: 2,
+      status: 'online'
     };
     let user3: User = {
       nick: 'Yuliana',
       age: 18,
       email: 'yuli@aoe.aoe',
       friend: true,
-      uid: 3
+      uid: 3,
+      status: 'offline'
     };
     let user4: User = {
       nick: 'Ricardo',
       age: 17,
       email: 'rick@aoe.aoe',
       friend: false,
-      uid: 4
+      uid: 4,
+      status: 'away'
     };
     let user5: User = {
       nick: 'Marcos',
       age: 30,
       email: 'marcos@aoe.aoe',
       friend: false,
-      uid: 5
+      uid: 5,
+      status: 'busy'
     };
 
     this.friends = [user1, user2, user3, user4, user5];
